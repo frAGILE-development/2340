039eb1add4b8001766d7c94053a2c9e530d1c037
bwatson35 <brycewatson315@gmail.com>
2017-06-25 14:41:52 -0400

    Created User class in model

---
+++ app/src/main/java/Model/User.java
@@ -0,0 +1,153 @@
+package Model;
+
+import android.os.Parcelable;
+import android.os.Parcel;
+
+import java.util.Arrays;
+import java.util.List;
+/**
+ * Created by bwatson35 on 6/25/17.
+ */
+
+public class User implements Parcelable{
+
+
+    /**
+     * Created by Bryce Watson on 6/25/17.
+     *
+     * Information Holder - represents a single user in model.
+     * Was modeled in part on the student class from Lab 3.
+     * We are passing this object in a bundle between intents, so we implement
+     * the Parcelable interface.
+     */
+
+        //instance variables
+        private int _id;
+        private String _firstName;
+        private String _lastName;
+        private String _password;
+        private String _email;
+        private String _phoneNumber;
+        private Boolean _admin;
+
+        //derived instance vars
+        private String _lastFirst;
+        private String _fullName;
+
+        /**
+         * Constructor for the user class
+         * @param firstName the user's first name
+         * @param lastName the user's last name
+         * @param password their password
+         * @param email their email address
+         * @param phoneNumber their phone number
+         * @param admin whether or not they are an administrator
+         */
+        public User(String firstName, String lastName, String password, String email,
+                       String phoneNumber, Boolean admin) {
+            _firstName = firstName;
+            _lastName = lastName;
+            _lastFirst = lastName + ", " + firstName;
+            _fullName = firstName + " " + lastName;
+            _password = password;
+            _email = email;
+            _phoneNumber = phoneNumber;
+            _admin = admin;
+        }
+
+        /**
+         * No param constructor -- DO NOT CALL NORMALLY
+         * This constructor only for GUI use in edit/new user dialog
+         */
+        public User() {
+            this("George" , "P. Burdell", "password", "george@gatech.edu", "0123456789", false);
+        }
+
+        /* **********************
+         * Getters and setters
+         */
+        public String getFirstName() { return _firstName; }
+        public void setFirstName(String s) { _firstName = s; }
+
+        public String getLastName() { return _lastName; }
+        public void setLastName(String s) { _lastName = s; }
+
+        public String getLastFirst() { return _lastFirst; }
+
+        public String getFullName() { return _fullName; }
+        public void setFullName(String first, String last) {
+            _firstName = first;
+            _lastName = last;
+        }
+
+        //TODO:no setter for password at the moment
+        public String getPassword() { return _password; }
+
+        public String getEmail() { return _email; }
+        public void setEmail(String s) { _email = s; }
+
+        public String getPhoneNumber() { return _phoneNumber; }
+        public void setPhoneNumber(String s) { _phoneNumber = s; }
+
+        public Boolean isAdmin() { return _admin; }
+        public void makeAdmin() { _admin = true; }
+        public void demoteToUser() { _admin = false; }
+
+
+        /**
+         *Prints the users full name, email, phone number, and whether they have admin access
+         * @return the display string representation
+         */
+        @Override
+        public String toString() {
+            return "User: " + _fullName + " Email: " + _email + " Phone: " + _phoneNumber
+                   + " Admin : " + _admin;
+        }
+
+
+    /* *********************************
+     * These methods are required by the parcelable interface
+     *
+     */
+
+        private User(Parcel in) {
+            _firstName = in.readString();
+            _lastName = in.readString();
+            _password = in.readString();
+            _email = in.readString();
+            _phoneNumber = in.readString();
+        }
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+
+        /* *************************
+        /  If you add new instance vars to Student, you will need to add them to the write
+        /*/
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            dest.writeString(_firstName);
+            dest.writeString(_lastFirst);
+            dest.writeString(_password);
+            dest.writeString(_email);
+            dest.writeString(_phoneNumber);
+
+            //dest.writeInt(_id);
+        }
+
+        public static final Parcelable.Creator<User> CREATOR
+                = new Parcelable.Creator<User>() {
+            public User createFromParcel(Parcel in) {
+                return new User(in);
+            }
+
+            public User[] newArray(int size) {
+                return new User[size];
+            }
+        };
+
+
+}
